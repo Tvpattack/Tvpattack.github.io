9309d62cea06c9ebb788e03bfdca69cc3e83d42f
Tvpattack <99218059+Tvpattack@users.noreply.github.com>
2025-12-26 01:32:22 -0500

    Auto-Push

--- scripts/print_html_for_index.py
+++ scripts/print_html_for_index.py
@@ -207,7 +207,7 @@ def generateHTML():
 			</select>
 		</div>
 		<div class="item-container">
-			<img class="banner" src="img/banner.png"></img>
+			<img class="banner" src="img/banner.gif"></img>
 			<input type="text" inputmode="search" placeholder="Search ..." autofocus="autofocus" name="search" id="search" spellcheck="false" autocomplete="off" autocorrext="off" spellcheck="false">
 			<div class="button-grid">
 				<button onclick="goToSets()"><img src="/img/sets.png" class="btn-img">All Sets</button>
