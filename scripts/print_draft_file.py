2a02567ee377ccbbacdd3b1f40a3957398b9822c
Tvpattack <Tvpattack@gmail.com>
2026-02-15 00:37:19 -0500

    Auto-Push

--- scripts/print_draft_file.py
+++ scripts/print_draft_file.py
@@ -59,7 +59,7 @@ def generateFile(code):
 						if ('!' + slot_name) in card['notes']:
 							booster[slot_name].append(card)
 
-			foil = "true" if suffix == "_foil" else "false"
+			foil = "true" if suffix == "_foil" else false
 
 			draft_string += '''	{
 				"name": "''' + card['card_name'] + suffix + '''",
